8c6e5816762368b41a18eeefcfb436144bc262c9
Tony Glover <tony.glover@nottingham.ac.uk>
2014-10-03 00:05:12 +0100

    Formatting and highlighting

--- js/horror.js
+++ js/horror.js
@@ -3,8 +3,35 @@
  */
 //Horror related stuff
 
+function updateClock() {
+    //Get current time
+    var date = new Date();
+    //Day
+    var day = date.getDate();
+    if(day < 10) day = '0'+day;
+    $('#day').html(day+':');
+    //Month
+    var month = date.getMonth()+1;
+    if(month < 10) month = '0'+month;
+    $('#month').html(month+':');
+    //Hour
+    var hour = date.getHours();
+    if(hour < 10) hour = '0'+hour;
+    $('#hour').html(hour+':');
+    //Minutes
+    var minutes = date.getMinutes();
+    if(minutes < 10) minutes = '0'+minutes;
+    $('#minute').html(minutes);
+}
 
 $(document).ready(function() {
     //Initialise app
+    updateClock();
+    setInterval(updateClock, 1000*60);
 
+    //Highlighting
+    $('li.filmList').on('click', function(evt) {
+        console.log(evt);
+        $('li.filmList').addClass('highLight');
+    })
 });
