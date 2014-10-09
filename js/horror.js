3afa1d7cebcab49f6eb3a67082132bf709bede55
Tony Glover <tony.glover@nottingham.ac.uk>
2014-10-10 00:20:34 +0100

    layout adjustments

--- js/horror.js
+++ js/horror.js
@@ -23,7 +23,7 @@ var films = (function() {
             "Hammer horror with Christopher Lee and Barbera Shelley where four travellers go on holiday to" +
             "Transylvania......"];
 
-    var filmNames = ['The Exorcist', 'The Texas Chainsaw', 'The Sound of Music', 'Count Dracula'];
+    var filmNames = ['The Exorcist', 'Frankensteins Mon', 'The Sound of Music', 'Count Dracula'];
     var filmTimes = ['31/10 22:00', '31/10 23:45', '01/11 20:00', '01/11 22:00'];
     var filmStatus = ['LIVE', 'PENDING', 'PENDING', 'PENDING'];
     var filmRecords = [];
